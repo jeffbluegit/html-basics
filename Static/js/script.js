0990a11b1a4ef874e0d67b7875479ca310ed174c
jeffbluegit <3mila19@gmail.com>
2026-03-04 10:10:11 +0300

    js

---
+++ Static/js/script.js
@@ -0,0 +1,6 @@
+function changecolor(params) {
+    document.getElementById("mypara").style.color="violet".background.color="fen"
+}
+function displayalert(params) {
+    alert("This is an alert")
+}
